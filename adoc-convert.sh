22d90c7e0b2bd0e7d68d339667f2f091a23b187b
Pawissanutt <pawissanutt@gmail.com>
2022-10-18 21:01:16 -0500

    Rename _README.adoc to let GitHub shows README.md

--- adoc-convert.sh
+++ adoc-convert.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 # apt install pandoc asciidoctor
-asciidoctor -b docbook README.adoc
+asciidoctor -b docbook _README.adoc
 pandoc -f docbook -t gfm README.xml -o README.md
+
+# Unicode symbols were mangled in foo.md. Quick workaround:
+$ iconv -t utf-8 README.xml | pandoc -f docbook -t gfm | iconv -f utf-8 > README.md
